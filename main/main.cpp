e20d5cf5d7041b8dcdfe0abd4b456dde855e2327
tabacaruandrei <35452862+tabacaruandrei@users.noreply.github.com>
2020-12-18 22:50:08 +0200

    Add files via upload
    Release 0.2
    - rotare cu coordonate memorate pe o imagine prin cstring
    - mutare cu coordonate memorate intr-un loc anumit
    - rularea jocului intr-o singura fereastra (ideea lui Leonard Rumeghea)
    - model pentru buton de intoarcere in desen
    - fereastra "despre" din meniul de start finalizata
    - operatiile de click din meniu mutate in void propriu "clickmeniu"
    - urmeaza sa fac operatiile de rotire si mutare pentru fiecare piesa de puzzle in parte si sa mut mare parte din cod in structuri proprii

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
+#include<iostream>
 #include<cstring>
 #include<graphics.h>
 #include<winbgim.h>
 using namespace std;
 
-int x,y;
 int area;
 int click;
 void *p;
@@ -14,62 +13,90 @@ char* f2="fig_2.jpg";
 char* f3="fig_3.jpg";
 char* fb="fig_b.jpg";*/
 
+int cx[50],cy[50],mx[50],my[50]; // x, y, marime x, marime y
+
+//retinem un vector pt rotire
+char f1[12]="fig_1_1.jpg", f2[12]="fig_2_1.jpg", f3[12]="fig_3_1.jpg", f4[12]="fig_b_1.jpg";
+
+int i,j; // in caz ca am for-uri
+
+struct
+{
+    int x,y;
+}m; // mouse
+
 void meniu()
 {
     //lucrat de Victoria
     //readimagefile("fundal.jpg",0,0,1500,600);
     //setcolor(BLACK);
+    cleardevice();
     settextstyle(8,0,3);
     settextjustify(1,1);
-    rectangle(500,100,700,175);
-    outtextxy(600,140,"START");
-    rectangle(500,225,700,300);
-    outtextxy(600,265,"NIVELURI");
-    rectangle(500,350,700,425);
-    outtextxy(600,390,"REGULI");
-    rectangle(500,475,700,550);
-    outtextxy(600,515,"EXIT");
-
+    rectangle(500,100,700,150);
+    outtextxy(600,130,"START");
+    rectangle(500,175,700,225);
+    outtextxy(600,205,"NIVELURI");
+    rectangle(500,250,700,300);
+    outtextxy(600,280,"DESPRE");
+    rectangle(500,325,700,375);
+    outtextxy(600,355,"REGULI");
+    rectangle(500,400,700,450);
+    outtextxy(600,430,"EXIT");
 }
 
 void desen()
 {
-//lucrat de amandoi
-initwindow(1500,600);
-readimagefile("fundal.jpg",0,0,1900,1280);
-readimagefile("tabla_joc.jpg",400,0,934,533);
-readimagefile("ex.jpg",100,200,373,528);
-readimagefile("fig_1.jpg",1100,200,1250,350);
-readimagefile("fig_2.jpg",1100,400,1250,550);
-readimagefile("fig_3.jpg",1300,200,1450,350);
-readimagefile("fig_b.jpg",1300,400,1450,550);
+    //lucrat de amandoi
+    //initwindow(1500,600);
+    cleardevice();
+    readimagefile("fundal.jpg",0,0,1900,1280);
+    readimagefile("tabla_joc.jpg",400,0,934,533);
+    readimagefile("ex.jpg",100,200,373,528);
+    readimagefile("fig_1_1.jpg",1100,200,1250,350);
+    cx[1]=1100;
+    cy[1]=200;
+    mx[1]=150;
+    my[1]=150;
+    readimagefile("fig_2.jpg",1100,400,1250,550);
+    readimagefile("fig_3.jpg",1300,200,1450,350);
+    readimagefile("fig_b.jpg",1300,400,1450,550);
+    rectangle(1250,50,1450,150);
 }
+
+
 
 void mutare()
 {
     // preluat din modelul razboi in 8 dat de domnul Patrut
-    if(x>=1100 && x<=1250 && y>=200 && y<=350)
+    if(m.x>=cx[1] && m.x<=cx[1]+mx[1] && m.y>=cy[1] && m.y<=cy[1]+my[1])
     {
-        getimage(1100, 200, 1250, 350, p);
+        getimage(cx[1], cy[1], cx[1]+mx[1], cy[1]+my[1], p);
         putimage(600, 200, p, 0);
-        readimagefile("fundal.jpg",1100,200,1250,350);
-        click=true;
+        readimagefile("fundal.jpg",cx[1],cy[1],cx[1]+mx[1],cy[1]+my[1]);
+        cx[1]=600;
+        cy[1]=200;
     }
 }
 
 void rotire()
 {
-    // lucrat de Andrei
-    if(x>=1100 && x<=1250 && y>=200 && y<=350)
+    //lucrat de Andrei
+    //1100, 200, 1250, 350
+    if(m.x>=cx[1] && m.x<=cx[1]+mx[1] && m.y>=cy[1] && m.y<=cy[1]+my[1])
     {
-        readimagefile("fig_1_drp.jpg",1100,200,1250,350);
+        //rotarea e memorata prin numere; 1 - normal, 2 - dreapta, 3 - jos, 4 - stanga
+        if(f1[strlen(f1)-5]=='4')
+            f1[strlen(f1)-5]='0';
+        f1[strlen(f1)-5]++;
+        readimagefile(f1,cx[1],cy[1],cx[1]+mx[1],cy[1]+my[1]);
     }
+
 }
 
 void reguli()
 {
     // lucrat de Victoria
-    initwindow(1500,600);
     settextstyle(8,0,2);
     settextjustify(1, 1);
     outtextxy(600, 100, "Pasul 1");
@@ -84,7 +111,6 @@ void reguli()
 void despre()
 {
     // lucrat de Victoria
-    initwindow(1500,600);
     settextstyle(8, 0, 3);
     settextjustify(1, 1);
     outtextxy(600, 80, "Categorie:  Jocuri compacte");
@@ -95,73 +121,85 @@ void despre()
     outtextxy(600, 480, "1 tabla de joc, 4 piese de puzzle, 1 brosura cu 20 de provocari");
 }
 
+void clickmeniu()
+{
+    if(m.x>=500 && m.x<=700)
+    {
+        if(m.y>=100 && m.y<=150)
+        {
+            desen();
+            click=true;
+        }
+        if(m.y>=175 && m.y<=225)
+        {
+            //niveluri();
+            click=true;
+        }
+        if(m.y>=250 && m.y<=300)
+        {
+            despre();
+            click=true;
+        }
+        if(m.y>=325 && m.y<=375)
+        {
+            reguli();
+            click=true;
+        }
+    }
+}
+
 int main()
 {
-//declaram fereastra de inceput
-initwindow(1500,600);
-meniu();
+    //declaram fereastra de inceput
+    initwindow(1500,600);
+    meniu();
 
-// in lucru
-// trebuie gasita o modalitate prin care iesim din meniu si intram in alta fereastra dupa click
-/*
-if(ismouseclick(WM_LBUTTONDOWN))
-{
-if(x>=500 && x<=700 && y>=100 && y<=175)
-        desen();
-//if(x>=500 && x<=700 && y>=225 && y<=300)
-//if(x>=500 && x<=700 && y>=350 && y<=425)
-//if(x>=500 && x<=700 && y>=475 && y<=550)
-}
-*/
+    // in lucru
+    // trebuie gasita o modalitate prin care iesim din meniu si intram in alta fereastra dupa click
     do{
         if(ismouseclick(WM_LBUTTONDOWN) && !click)
-        {
+        { // ideea lui Leonard
             clearmouseclick(WM_LBUTTONDOWN);
-            x=mousex(); y=mousey();
-            if(x>=500 && x<=700 && y>=100 && y<=175)
-            {
-                desen();
-                click=true;
-            }
-            if(x>=500 && x<=700 && y>=225 && y<=300)
-            {
-                //niveluri();
-                click=true;
-            }
-            if(x>=500 && x<=700 && y>=350 && y<=425)
-            {
-                reguli();
-                click=true;
-            }
-            if(x>=500 && x<=700 && y>=475 && y<=550)
+            m.x=mousex(); m.y=mousey();
+            clickmeniu();
+            if(m.x>=500 && m.x<=700 && m.y>=400 && m.y<=450)
             {
                 return 0;
             }
         }
     }while(!click);
-click=false;
+    click=false;
+// scot vectori x, y, marime pentru figurile de puzzle
+    area = imagesize(1100, 200, 1250, 350);
+    p = malloc(area);
 
-area = imagesize(1100, 200, 1250, 350);
-p = malloc(area);
+    // sa combin do ... while(!click)-urile intr-unul singur
+    // pun operatiile in voiduri tip clickmeniu, de ex clickdesen
 
     do
     {
         if(ismouseclick(WM_LBUTTONDOWN) && !click)
         {
             clearmouseclick(WM_LBUTTONDOWN);
-            x=mousex(); y=mousey();
+            m.x=mousex(); m.y=mousey();
             mutare();
+            //model pentru butonul de intoarcere; momentan nu apare
+            if(m.x>=1250 && m.x<=1450 && m.y>=50 && m.y<=150)
+            {
+                click=true;
+                meniu();
+            }
         }
         if(ismouseclick(WM_RBUTTONDOWN) && !click)
         {
             clearmouseclick(WM_RBUTTONDOWN);
-            x=mousex(); y=mousey();
+            m.x=mousex(); m.y=mousey();
             rotire();
         }
     }while(!click);
-click=false;
- getch();
- closegraph();
- return 0;
+    click=false;
 
+    getch();
+    closegraph();
+    return 0;
 }
